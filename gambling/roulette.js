69130bbd2f90e6074813abc04ce508ea50aae851
blackkuba <kubastopa59@gmail.com>
2024-03-21 19:05:55 +0100

    huge changesz

---
+++ gambling/roulette.js
@@ -0,0 +1,18 @@
+
+
+const arrayRed = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36];
+
+function result_style(){
+    let result = Math.floor(Math.random()  * 36);
+
+    if(result == 0){
+        document.getElementById("spiner_num").style.backgroundColor = "#3a8f3d";
+    }else if(arrayRed.includes(result)){
+         document.getElementById("spiner_num").style.backgroundColor = "#ff394a";
+    }else{
+        document.getElementById("spiner_num").style.backgroundColor = "#2c4452";
+    }
+
+    document.getElementById("spiner_num").innerHTML = result;
+};
+
